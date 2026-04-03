a3b3611184187fb01f2a7a2d73282f51a0c02c46
Prashant Singh <143212076+prashant290605@users.noreply.github.com>
2026-04-03 10:10:13 +0530

    Add: non-cyclical-number - submission-1

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        hash = set()
+        s = str(n)
+        while True:
+            val = 0
+            for i in s:
+                j = int(i)
+                val += j**2
+            if val in hash:
+                return False
+            if val == 1:
+                return True
+            else:
+                s = str(val)
+                hash.add(val)
