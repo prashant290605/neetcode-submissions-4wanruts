4de03df6ccf90c75ef20a8ec21b2b3313af3d04c
Prashant Singh <143212076+prashant290605@users.noreply.github.com>
2026-04-06 09:05:40 +0530

    Add: longest-increasing-path-in-matrix - submission-0

---
+++ Data Structures & Algorithms/longest-increasing-path-in-matrix/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        m,n = len(matrix),len(matrix[0])
+        dp = {}
+
+        def dfs(r,c,preval):
+            if (r < 0 or r == m or c < 0 or c == n or matrix[r][c] <= preval):
+                return 0
+            
+            if (r,c) in dp:
+                return dp[(r,c)]
+            
+            res = 1
+            res = max(res , 1 + dfs(r+1,c,matrix[r][c]))
+            res = max(res , 1 + dfs(r-1,c,matrix[r][c]))
+            res = max(res , 1 + dfs(r,c+1,matrix[r][c]))
+            res = max(res , 1 + dfs(r,c-1,matrix[r][c]))
+            dp[(r,c)] = res
+            return res
+        
+        for r in range(m):
+            for c in range(n):
+                dfs(r,c,-1)
+        return max(dp.values())
